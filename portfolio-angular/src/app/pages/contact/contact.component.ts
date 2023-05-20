ce6fcadb3e97cd014c4787d0c630cba04b40c8ef
ScordovaB <98288513+ScordovaB@users.noreply.github.com>
2023-05-19 21:49:57 -0600

    contact form

--- portfolio-angular/src/app/pages/contact/contact.component.ts
+++ portfolio-angular/src/app/pages/contact/contact.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  sendText:string ='Send';
 
+  constructor(){
+
+  }
+  
+
+  send(){
+    this.sendText = 'Sending...';
+    setTimeout(()=>{
+      this.sendText='Send';
+    },3000);
+  }
 }
